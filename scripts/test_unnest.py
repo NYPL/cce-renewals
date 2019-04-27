7aa760b4311b7164617a226122c7fa4ab7e153ff
Sean Redmond <github-smr@sneakemail.com>
2019-04-27 09:58:12 -0400

    Use a here document for input

--- scripts/test_unnest.py
+++ scripts/test_unnest.py
@@ -2,15 +2,24 @@ import unnest
 import parse
 
 
+def split_in(s):
+    return [line.rstrip() for line in s.split('\n')]
+
+    
 def join_out(s):
     return '\t'.join(s) + '\n'
 
+
 def test_basic_entry(capsys):
-  l1 = 'ACTUAL BUSINESS ENGLISH, by P. H. Deffendall.'
-  l2 = '  © 1Aug22, A681161. R60449,'
-  l3 = '  5Apr50, P. H. Deffendall (A)'
-  l4 = ''
-  l5 = ''
+  inp = '''\
+ACTUAL BUSINESS ENGLISH, by P. H. Deffendall.
+  © 1Aug22, A681161. R60449,
+  5Apr50, P. H. Deffendall (A)
+
+\
+'''
+
+  l = split_in(inp)
 
   expected = join_out((
       '7a3120c6-1d31-5715-942e-b38216e86c13', '1', '1',
@@ -27,19 +36,19 @@ def test_basic_entry(capsys):
        'number': 1
   }
 
-  s = unnest.transition(unnest.TRANSITIONS, s, l1)
+  s = unnest.transition(unnest.TRANSITIONS, s, l[0])
   assert s['state'] == unnest.State.entry
 
-  s = unnest.transition(unnest.TRANSITIONS, s, l2)
+  s = unnest.transition(unnest.TRANSITIONS, s, l[1])
   assert s['state'] == unnest.State.continuing
 
-  s = unnest.transition(unnest.TRANSITIONS, s, l3)
+  s = unnest.transition(unnest.TRANSITIONS, s, l[2])
   assert s['state'] == unnest.State.continuing
   
-  s = unnest.transition(unnest.TRANSITIONS, s, l4)
+  s = unnest.transition(unnest.TRANSITIONS, s, l[3])
   assert s['state'] == unnest.State.blank
 
-  s = unnest.transition(unnest.TRANSITIONS, s, l5)
+  s = unnest.transition(unnest.TRANSITIONS, s, l[4])
   assert s['state'] == unnest.State.start
 
   out, err = capsys.readouterr()
