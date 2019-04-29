e4a3f0106eaac4fa99e95a7ac475b857e79c48b2
Sean Redmond <github-smr@sneakemail.com>
2019-04-29 14:52:27 -0400

    Add comments

--- scripts/parse.py
+++ scripts/parse.py
@@ -1,10 +1,30 @@
 #!/usr/bin/env python3 -u
 
+# Take tab delimited-files produced by unnest.py and parse and
+# "unroll" the entries. "Unrolling" means to take a row that really
+# representes multiple renewals and convert to as many rows as
+# necessary, keeping all the numbers and dates corrdinated.
+#
+# Parsing might be done to one of two levels. If the author, title,
+# claimants, etc. can be determined, these are all broken out into
+# individual fields.
+#
+# If the individual fields can't be determined but we can try to
+# extract at least the id numbers and dates while leaving the rest of
+# the fields blank.
+#
+# If neither of the above is possible, just output the metadata fields
+# and full text already in the unnested file, with the other fields
+# blank.
+
 import argparse
 import csv
 import dateutil.parser
 import re
 import sys
+
+# Let a hundred regexes bloom...
+# These handle all the patterns we are looking to to parse the entries
 
 DATE_RE = r'\d{1,2}[A-z]{3}\d{1,2}'
 ONE_DATE = re.compile(DATE_RE)
@@ -24,6 +44,10 @@ PUB_ABROAD_RE = r'\(pub\. abroad.+?\)[,;]'
 NEW_MATTER_RE = r'(?:on (?:.+?);)?'
 
 
+# We have a number of regexes that look to extract certain kinds of
+# info. This is a generic way to create a regex for each that just
+# looks for it at the beginning if a line, User by the various
+# shift_X() functions
 def shift_re(r):
     return re.compile(r'^(%s)(.*)$' % r)
 
@@ -66,6 +90,7 @@ AUTH_TITLE = re.compile(r'^(.*)([,;] by )((?:[^\(;](?!Pub. ))+)(.*)$')
 AUTH_TITLE_F3 = re.compile(r'^(.+[\.\)])( By )((?:[^\(])+)(.*)$')
 
 
+# Return a record that is blank except for the fields supplied
 def record(**kwargs):
     return {**{'author': None, 'title': None, 'oreg': None,
                'odat': None, 'id': None, 'rdat': None,
@@ -74,11 +99,16 @@ def record(**kwargs):
                'see_also_reg': None}, **kwargs}
 
 
+# Extract info from the beginning of a string. Pattern for the info is
+# in the passed in regex. by default return the extract info and the
+# remainder of the string, but if a function is supplied, run that
+# function with the extracted info and remainder and return the result
 def shift_field(s, r, op=lambda x, y: (x, y)):
     m = r.match(s)
     return op(*(m[2].lstrip(';., '), [m[1]]))
 
 
+# Various functions to extract particular info from the beginning of a string
 def shift_dates(s):
     return shift_field(s, SHIFT_DATES, extract_dates)
 
@@ -117,6 +147,11 @@ def shift_claims(s):
             '||'.join(format_claims(split_on_codes(CODE_SPLIT.split(m[1])))))
 
 
+# Convert dates to ISO-8601 format. Since dates in the renewals have
+# only two digit years, the result may need to have 100 years
+# subtracted.
+#
+# Return None if the date is invalid
 def parse_date(d):
     try:
         dt = dateutil.parser.parse(d)
@@ -130,6 +165,7 @@ def parse_date(d):
         return None
 
 
+# Various functions to extract info from strigs
 def extract_dates(reg, dates):
     return (reg, [parse_date(d) for d in ONE_DATE.findall(dates[0])])
 
@@ -146,20 +182,6 @@ def extract_date_reg_pairs(reg, pairs):
 
 def extract_rids(reg, rids):
     return (reg, unroll_rids(EXTRACT_RIDS.findall(rids[0])))
-
-
-def unroll_rids(rids):
-    """Recursively unroll rid id ranges into individual rids."""
-    if not rids:
-        return []
-
-    m = rids[0]
-    if m[1]:
-        return ['R%d' % r for r in range(int(m[0]),
-                                         int(m[1])+1)] + unroll_rids(rids[1:])
-    return ['R%s' % m[0]] + unroll_rids(rids[1:])
-
-    return []
 
 
 def extract_interim(reg, r):
@@ -189,6 +211,23 @@ def extract_see_also(reg, r):
     return (reg, rids, regnums)
 
 
+# Turn a text list of renewal ids into a Python list of renewal IDs
+# and extrapolate ranges if necessary
+def unroll_rids(rids):
+    """Recursively unroll rid id ranges into individual rids."""
+    if not rids:
+        return []
+
+    m = rids[0]
+    if m[1]:
+        return ['R%d' % r for r in range(int(m[0]),
+                                         int(m[1])+1)] + unroll_rids(rids[1:])
+    return ['R%s' % m[0]] + unroll_rids(rids[1:])
+
+    return []
+
+
+# Join two parts of a title with proper spacing
 def join_title_parts(p1, p2):
     if p2:
         if p2[1] == ' ':
@@ -197,6 +236,7 @@ def join_title_parts(p1, p2):
     return p1.strip()
 
 
+# Look for '1st ed', '3rd ed., rev.', etc.
 def find_numbered_eds(author, title):
     m = re.match(r'^(.+)( [0-9]+(?:[nr]?d|th).+ed\.(?:, rev\.)?)$', author)
     if m:
@@ -204,6 +244,7 @@ def find_numbered_eds(author, title):
     return (author, title)
 
 
+# Try to extract the author and title from an entry
 def get_author_title(book):
     m = AUTH_TITLE.match(book)
 
@@ -219,6 +260,7 @@ def get_author_title(book):
     return (None, book)
 
 
+# Try to extract 'new matter' notice (format 3)
 def get_f3_new_matter(book):
     m = re.match(r'^(.+)(?:NM: )(.+)$', book)
     if m:
@@ -226,6 +268,7 @@ def get_f3_new_matter(book):
     return (book, None)
 
 
+# Look for information that sometimes comes after the author
 def find_post_author(author, title):
     m = re.match(r'^(.+)(Prev\. pub\. .+)$', author)
     if m:
@@ -233,6 +276,7 @@ def find_post_author(author, title):
     return (author, title)
 
 
+# Try to extract the author and title from an entry (format 3)
 def get_f3_author_title(book):
     m = AUTH_TITLE_F3.match(book)
     if m:
@@ -243,6 +287,7 @@ def get_f3_author_title(book):
     return (None, book)
 
 
+# Look for pairs of interim registration ids and dates
 def interim_pairs(s, p):
     dates = [parse_date(d) for d in ONE_DATE.findall(p)]
     numbers = ONE_AI.findall(p)
@@ -257,31 +302,30 @@ def interim_pairs(s, p):
     raise Exception('Cannot parse interim registrations %s' % p)
 
 
+# Format one interim registration for output
 def one_ai(s, p):
     return '|'.join([s, p[0], p[1]])
 
 
+# Pad out the list if the code is missing, otherwise trim the last
+# empty item.
 def split_on_codes(c):
-    """
-    Pad out the list if the code is missing, otherwise trim the last
-    empty item.
-
-    """
     return (len(c) == 1 and (c[0], '')) or c[0:-1]
 
 
+# Format claimants and code for output
 def format_claims(c):
     return (c and ('|'.join(cleanup_claim(c[0:2])),) +
             format_claims(c[2:])) or ()
 
 
+# Remove leading punctuation and whitespace from claimant strings
 def cleanup_claim(c):
-    """Remove leading punctuation and whitespace from claimant strings."""
     return (c[0].lstrip(',.& ').strip(), c[1])
 
 
+# Recursively unroll rid id ranges into individual rids.
 def unroll_regnums(regs):
-    """Recursively unroll rid id ranges into individual rids."""
     if not regs:
         return []
 
@@ -299,28 +343,30 @@ def unroll_regnums(regs):
     return []
 
 
+# Test whether the dates and ids are all single items
 def all_singles(regdates, regnums, rids, rendates, interims):
     return len(regdates) == len(regnums) == len(rids) == \
         len(rendates) == 1 and (interims is None or len(interims) == 1)
 
 
+# Test whether all the dates and ids are single items except for the
+# interim registrations
 def all_singles_but_interims(regdates, regnums, rids, rendates):
     return len(regdates) == len(regnums) == len(rids) == \
         len(rendates) == 1
 
 
+# Test whether all these are lists of the same length, except for
+# interims which must either have the same length as the others or be
+# None.
 def all_multiples(regdates, regnums, rids, rendates, interims):
-    """All these are lists of the same length, except for interims which
-       either has the same length as the others or is None."""
-
     return len(regdates) == len(regnums) == len(rids) == \
         len(rendates) and (interims is None or len(interims) == len(regdates))
 
 
+# Test wheter all these lists are as long as the longest or only have
+# 1 item, except for interim registrations which might also be None
 def most_multiples(regdates, regnums, rids, rendates, interims):
-    """All these are lists of the same length, except for interims which
-       either has the same length as the others or is None."""
-
     max_len = max([len(l) for l in (regdates, regnums, rids, rendates)])
     return n_or_1(max_len, regdates) and \
         n_or_1(max_len, regnums) and \
@@ -329,14 +375,17 @@ def most_multiples(regdates, regnums, rids, rendates, interims):
         n_1_or_none(max_len, interims)
 
 
+# The length of list is either n or 1
 def n_or_1(n, l):
     return len(l) in (1, n)
 
 
+# The lenght of list l is n, 1, or None
 def n_1_or_none(n, l):
     return l is None or n_or_1(n, l)
 
 
+# Remove hyphens from class AI regnums
 def dehyphen(r):
     m = re.match(r'^AI\-(\d+)$', r)
     if m:
@@ -345,6 +394,7 @@ def dehyphen(r):
     return r
 
 
+# Format a record for output
 def format_record(author=None, title=None, regdates=None, regnums=None,
                   rids=None, rendates=None, claims=None, notes=None,
                   previous=None, new_matter=None, see_also_ren=None,
@@ -386,6 +436,9 @@ def format_record(author=None, title=None, regdates=None, regnums=None,
     return False
 
 
+# Our record may in fact be multiple records if the list of regnums,
+# etc. has more than 1 item. Pad multiply one item lists to the length
+# of the multiple item lists and return as many records as necessary
 def pad_and_unroll_records(author=None, title=None, regdates=None,
                            regnums=None, rids=None, rendates=None,
                            claims=None, notes=None, previous=None,
@@ -406,12 +459,17 @@ def pad_and_unroll_records(author=None, title=None, regdates=None,
                 [notes] * max_len)]
 
 
+# Given a list l and a length n make sure that l has length n, or if l
+# has length 1, multiply it to match length n.
+#
+# Return False if l has any length either than 1 or n.
 def unroll_to(n, l):
     return (len(l) == n and l) or \
         len(l) == 1 and l * n or \
         False
 
 
+# Add metadata fields to the record.
 def add_metadata(p, r):
     return {**p,
             **{'full_text': ' '.join(r[-1].split('|'))},
@@ -419,28 +477,32 @@ def add_metadata(p, r):
                        r[0:5]))}
 
 
+# The format of the CCE renewals changed over time. Check the volume
+# and number of the entry to determine if we should use format 1, 2, or 3
+
+# Does the volume number correspond to format 1?
 def format1(v):
-    """Does the volume number correspond to format 1?"""
     return v in ('4', '5', '6', '7')
 
 
+# Does the volume number correspond to format 2?
 def format2(v, p):
-    """Does the volume number correspond to format 2?"""
     return v in ('8', '9', '10', '11', '12', '13', '14', '15', '16', '17',
                  '18', '19', '20', '21', '22', '23', '24', '25', '26') \
         or (v == '27' and p == '1')
 
 
+# Does the volume number correspond to format 3?
 def format3(v, p):
-    """Does the volume number correspond to format 2?"""
     return (v == '27' and p == '2') or v in ('28', '29', '30', '31')
 
 
+# Split on ©-symbol.
 def cc_split(e):
-    """Split on ©-symbol."""
     return [s.strip() for s in e.split('©')]
 
 
+# Last ditch, just extract the numbers (format 1)
 def f1_just_numbers(e):
     reg_date = re.findall(DATE_REG_PAIR_RE, e)
     if len(reg_date):
@@ -457,6 +519,7 @@ def f1_just_numbers(e):
     return False
 
 
+# Last ditch, just extract the numbers (format 2)
 def f2_just_numbers(e):
     reg_date = re.findall(DATE_REG_PAIR2_RE, e)
     if len(reg_date):
@@ -479,6 +542,8 @@ def f2_just_numbers(e):
     return False
 
 
+# Figure out which format we are dealing with and dispatch to the
+# proper handler.
 def parse(v, p, e):
     """Dispatch to proper parsing function based on expected format."""
     return format1(v) and f1_parse(e) or \
@@ -487,6 +552,7 @@ def parse(v, p, e):
         [record()]
 
 
+# Try several ways of parsing a format 1 entry
 def f1_parse(e):
     """Dispatch to proper f1 parsing function based on number of parts."""
     p = e.split('|')
@@ -503,8 +569,8 @@ def f1_parse(e):
     return False
 
 
+# Simplest version of f1 format.
 def f1_one_part(e):
-    """Simplest version of f1 format."""
     try:
         book, reg = cc_split(e)
         author, title = get_author_title(book)
@@ -543,8 +609,8 @@ def f1_one_part(e):
         return False
 
 
+# F1 format with date/regnum pairs.
 def f1_date_reg_pairs(e):
-    """F1 format with date/regnum pairs."""
     try:
         book, reg = cc_split(e)
         author, title = get_author_title(book)
@@ -567,6 +633,9 @@ def f1_date_reg_pairs(e):
         return False
 
 
+# F1 in two parts. depending on which parts contain a ©-symbol,
+# rearrange the parts into something that can be parsed by another
+# handler
 def f1_two_parts(p1, p2):
     if '©' in p2:
         if '©' not in p1:
@@ -577,12 +646,14 @@ def f1_two_parts(p1, p2):
     return False
 
 
+# Rearrange the parts into a 1-part format
 def f1_f2_rearrange_two_parts(p1, p2):
     pre, post = cc_split(p2)
 
     return (p1.strip() + ' ' + pre.strip()).strip() + ' © ' + post.strip()
 
 
+# Take an 'A B B A' format and rearrange it to be more simple
 def f1_rearrange_chiastic(p1, p2):
     try:
         p1a, p1b = cc_split(p1)
@@ -594,6 +665,7 @@ def f1_rearrange_chiastic(p1, p2):
         return False
 
 
+# Handle a three part entry by rearranging it into a one-part entry
 def f1_three_parts(p1, p2, p3):
     if '©' in p2 and '©' in p3:
         return f1_rearrange_three_parts(p1, p2, p3)
@@ -611,8 +683,9 @@ def f1_rearrange_three_parts(p1, p2, p3):
 #
 # Parse Format 2 (vols 8-)
 #
+
+# Dispatch to proper f2 parsing function based on number of parts."""
 def f2_parse(e):
-    """Dispatch to proper f2 parsing function based on number of parts."""
     p = e.split('|')
     if len(p) == 1:
         return f2_one_part(*p) or f2_just_numbers(e)
@@ -626,8 +699,8 @@ def f2_parse(e):
     return False
 
 
+# Simplest version of format 2.
 def f2_one_part(e, author=None):
-    """Simplest version of format 2."""
     try:
         title, reg = cc_split(e)
 
